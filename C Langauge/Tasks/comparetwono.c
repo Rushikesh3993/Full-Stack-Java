7698f8b66aff3a86328651eb8ff33128368dd1a9
Rushikesh Mote <moterushikesh39@gmail.com>
2025-08-12 22:14:29 +0530

    compare the two number which is greater

---
+++ C Langauge/Tasks/comparetwono.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    int a = 10, b = 20;
+    if(a > b){
+        printf("a is largest Number: %d", a);
+    }else{
+        printf("b is largest Number: %d", b);
+    }
+}
