da83ee2afaa0ff50c511e4b3f212407cb68158f6
Rushikesh Mote <moterushikesh39@gmail.com>
2025-08-12 22:14:50 +0530

    compare the three number which is greater

---
+++ C Langauge/Tasks/comparethreeno.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    int a = 1, b = 1 , c = 1;
+    if(a > b && a > c){
+        printf("a is largest Number: %d", a);
+    }else if (b > c)
+    {
+        printf("b is largest Number: %d", b);
+    }else
+    {
+        printf("c is largest Number: %d", c);
+    }
+    
+    
+}
