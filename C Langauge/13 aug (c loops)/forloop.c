3ebbd3821a7f3a8bef3ccc62001e88266250f94e
Rushikesh Mote <moterushikesh39@gmail.com>
2025-08-13 10:34:45 +0530

    for loop

---
+++ C Langauge/13 aug (c loops)/forloop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    
+    // 1 to 10
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+
+    // 10 to 1
+    for (int i = 10; i >= 1; i--)
+    {
+        printf("\n%d", i);
+    }
+    
+    
+}
