9ec39aaf7abd35ee13ad150c7710219c80082dd1
Rushikesh Mote <moterushikesh39@gmail.com>
2025-08-18 23:17:55 +0530

    Swap program

---
+++ C Langauge/Tasks_c/Swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int swap(int *a, int *b){
+    printf("\nBefore swaping a is %d and b is %d ",*a,*b);
+    int temp = (*a);
+    (*a) = (*b);
+    (*b) = temp;
+    printf("\nAfter swaping a is %d and b is %d ",*a,*b);
+}
+
+void main(){
+
+    int a = 10 , b = 20;
+    swap(&a,&b);
+
+}
