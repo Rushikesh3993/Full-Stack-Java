329be5acb3b06b1a8e61b7cb2782745ff982e0e2
Rushikesh Mote <moterushikesh39@gmail.com>
2025-08-19 11:18:36 +0530

    another method of swap

--- C Langauge/19 Aug (array)/swap_arraynumbers.c
+++ C Langauge/19 Aug (array)/swap_arraynumbers.c
@@ -5,6 +5,12 @@ void main(){
 
   int num[5] = {10,23,43,23,11};
 
+//   another way to swap
+//   int first = num[0];
+//   int last = num[4];
+//   num[0] = last;
+//   num[4] = first;
+
   for (int i = 0; i < 5; i++)
   {
    int temp = num[0];
