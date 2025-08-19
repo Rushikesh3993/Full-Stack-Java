559ab3d9df24e61eea076f17691e29cafe4e717b
Rushikesh Mote <moterushikesh39@gmail.com>
2025-08-19 11:10:36 +0530

    array start in c

---
+++ C Langauge/19 Aug (array)/array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+
+    int num[5] = {10,23,43,23,11};
+
+    // num[0] = 10;
+    // num[1] = 100;
+    // num[2] = 40;
+    // num[3] = 30;
+    // num[4] = 20;
+
+    printf("%d", num[0]);
+}
